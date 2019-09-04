5f619376b21cf30750b06a9c948440364d8cfd4d
kevmargon <kvmago@gmail.com>
2019-09-04 21:06:11 +0200

    First Commit

---
+++ src/main/java/com/stockagent/StockAgentSpringApplication.java
@@ -0,0 +1,13 @@
+package com.stockagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAgentSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAgentSpringApplication.class, args);
+	}
+
+}
